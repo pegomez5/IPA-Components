8423f9d9fd8c3179c675515028d95ac28271b284
Pedro Gomez <fball.pedro@gmail.com>
2024-03-07 21:40:08 -0500

    Added dmux, rshift, xor, sub, & their respective testbenches.

--- alu_components.v
+++ alu_components.v
@@ -47,6 +47,64 @@ module and_word (
    end  
 
 endmodule
+module dmux(
+   input [19:0] I,
+   input [3:0] sel,
+   output reg [19:0] o0,
+   output reg [19:0] o1,
+   output reg [19:0] o2,
+   output reg [19:0] o3,
+   output reg [19:0] o4,
+   output reg [19:0] o5,
+   output reg [19:0] o6,
+   output reg [19:0] o7,
+   output reg [19:0] o8,
+   output reg [19:0] o9,
+   output reg [19:0] o10,
+   output reg [19:0] o11,
+   output reg [19:0] o12,
+   output reg [19:0] o13,
+   output reg [19:0] o14,
+   output reg [19:0] o15
+);
+
+always @(*) begin
+   o0= 0;
+   o1= 0;
+   o2= 0;
+   o3= 0;
+   o4= 0;
+   o5= 0;
+   o6= 0;
+   o7= 0;
+   o8= 0;
+   o9= 0;
+   o10= 0;
+   o11= 0;
+   o12= 0;
+   o13= 0;
+   o14= 0;
+   o15= 0;
+   case(sel)
+      4'b0000: o0= I;
+      4'b0001: o1= I;
+      4'b0010: o2= I;
+      4'b0011: o3= I;
+      4'b0100: o4= I;
+      4'b0101: o5= I;
+      4'b0110: o6= I;
+      4'b0111: o7= I;
+      4'b1000: o8= I;
+      4'b1001: o9= I;
+      4'b1010: o10= I;
+      4'b1011: o11= I;
+      4'b1100: o12= I;
+      4'b1101: o13= I;
+      4'b1110: o14= I;
+      4'b1111: o15= I;
+   endcase
+end
+endmodule 
 
 module or_word (
    input [19:0] a,
@@ -59,6 +117,20 @@ module or_word (
     // or operation on a,b
         c = a | b ;
     
+   end  
+
+endmodule
+
+module xor_word(
+   input [19:0] a,
+   input [19:0] b,
+   output reg [19:0] c,
+   output reg zero
+);
+
+   always @(a or b) begin
+        c = a ^ b;
+        zero = !(|c);
    end  
 
 endmodule
@@ -136,4 +208,14 @@ module swap (
       swap_b = a;
    end  
 
+endmodule
+module sub_word(
+   input [19:0] a,
+   input [19:0] b,
+   output reg [19:0] out
+);
+
+always @(a or b) begin
+   out = a-b;
+end
 endmodule
--- alu_tb.v
+++ alu_tb.v
@@ -39,6 +39,68 @@ module tb_mux;
 
 endmodule
 
+
+module tb_dmux;
+    // Set inputs and outputs
+    reg [19:0] I;
+    reg [3:0] sel;
+    wire [19:0] o0;
+    wire [19:0] o1;
+    wire [19:0] o2;
+    wire [19:0] o3;
+    wire [19:0] o4;
+    wire [19:0] o5;
+    wire [19:0] o6;
+    wire [19:0] o7;
+    wire [19:0] o8;
+    wire [19:0] o9;
+    wire [19:0] o10;
+    wire [19:0] o11;
+    wire [19:0] o12;
+    wire [19:0] o13;
+    wire [19:0] o14;
+    wire [19:0] o15;
+    
+
+    integer i;
+    integer MAX_ITERS = 25;
+    integer SEED = 10531;
+
+    // Instantiate the dmux
+    dmux x0 (
+        .I (I),
+        .sel (sel),
+        .o0 (o0),
+        .o1 (o1),
+        .o2 (o2),
+        .o3 (o3),
+        .o4 (o4),
+        .o5 (o5),
+        .o6 (o6),
+        .o7 (o7),
+        .o8 (o8),
+        .o9 (o9),
+        .o10 (o10),
+        .o11 (o11),
+        .o12 (o12),
+        .o13 (o13),
+        .o14 (o14),
+        .o15 (o15)
+    );
+
+    initial begin
+        $dumpfile("testbench_dmux.vcd");
+        $dumpvars(1,x0);
+        $monitor ("I: 0b%0b - sel: 0b%0b - o0: 0b%0b - o1: 0b%0b - o2: 0b%0b - o3: 0b%0b - o4: 0b%0b - o5: 0b%0b - o6: 0b%0b - o7: 0b%0b - o8: 0b%0b - o9: 0b%0b - o10: 0b%0b - o11: 0b%0b - o12: 0b%0b - o13: 0b%0b - o14: 0b%0b - o15: 0b%0b", I, sel, o0, o1, o2, o3, o4, o5, o6, o7, o8, o9, o10, o11, o12, o13, o14, o15);
+
+        for (i = 0; i < MAX_ITERS; i++) begin
+            #10 I <= $urandom(SEED);
+                sel <= $urandom(SEED);  
+        end
+    end
+
+endmodule
+
 module tb_or_word;
     // Set inputs and outputs
     reg [19:0] a;
@@ -64,6 +126,41 @@ module tb_or_word;
         $dumpfile("testbench.vcd");
         $dumpvars(1,x0);
         $monitor ("a: 0b%0b - b: 0b%0b - c: 0b%0b ", a, b,c);
+
+        for (i = 0; i < MAX_ITERS; i++) begin
+            #10 a <= $urandom(SEED);
+                b <= $urandom(SEED);
+        end
+    end
+
+endmodule
+
+module testbench_xor;
+    // Set inputs and outputs
+    reg [19:0] a;
+    reg [19:0] b;
+    wire [19:0] c;
+    wire zero;
+
+    integer i;
+    integer MAX_ITERS = 10;
+    integer SEED = 10531;
+
+    // Instantiate the XOR
+    xor_word x0 (
+        .a (a),
+        .b (b),
+        .c (c),
+        .zero (zero)
+    );
+
+    initial begin
+        a <= 0;
+        b <= 0;
+
+        $dumpfile("testbench_xor.vcd");
+        $dumpvars(1,x0);
+        $monitor ("a: 0b%0b - b: 0b%0b - c: 0b%0b - zero: 0b%0b", a, b, c, zero);
 
         for (i = 0; i < MAX_ITERS; i++) begin
             #10 a <= $urandom(SEED);
@@ -108,6 +205,37 @@ module tb_shift_left;
 
 endmodule
 
+
+module tb_rshift;
+    // Set inputs and outputs
+    reg [19:0] data_in;
+    reg [3:0] amount;
+    wire [19:0] out;
+
+    integer i;
+    integer MAX_ITERS = 10;
+    integer SEED = 10531;
+
+    // Instantiate the lshift
+    shift_right x0 (
+        .data_in (data_in),
+        .amount (amount),
+        .out (out)
+    );
+
+    initial begin
+        $dumpfile("testbench_lshift.vcd");
+        $dumpvars(1,x0);
+        $monitor ("data_in: 0b%0b - amount: 0b%0b - out: 0b%0b", data_in, amount, out);
+
+        for (i = 0; i < MAX_ITERS; i++) begin
+            #10 data_in <= $urandom(SEED);
+                amount <= $urandom(SEED);
+        end
+    end
+
+endmodule
+
 module tb_ADDER;
     // Set inputs and outputs
     reg [19:0] a;
@@ -142,6 +270,37 @@ module tb_ADDER;
             #10 a <= $urandom(SEED);
                 b <= $urandom(SEED);
                 c_in <= $urandom(SEED);
+        end
+    end
+
+endmodule
+
+
+module tb_subtractor;
+    // Set inputs and outputs
+    reg [19:0] a;
+    reg [19:0] b;
+    wire [19:0] out;
+
+    integer i;
+    integer MAX_ITERS = 10;
+    integer SEED = 10531;
+
+    // Instantiate the SUB
+    sub_word x0 (
+        .a (a),
+        .b (b),
+        .out (out)
+    );
+
+    initial begin
+        $dumpfile("testbench_subtractor.vcd");
+        $dumpvars(1,x0);
+        $monitor ("a: 0b%0b - b: 0b%0b - out: 0b%0b", a, b, out);
+
+        for (i = 0; i < MAX_ITERS; i++) begin
+            #10 a <= $urandom(SEED);
+                b <= $urandom(SEED);
         end
     end
 
