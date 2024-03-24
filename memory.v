df9a95a329a252f2b8e67dd261c4a44ec3750253
TrulyElijah <131791100+TrulyElijah@users.noreply.github.com>
2024-03-23 22:23:25 -0400

    implemented memory file and included it into urcpu

---
+++ memory.v
@@ -0,0 +1,58 @@
+module MRR (
+    input clock,
+    input [19:0] Ro,
+    input [19:0] Rd,
+    input [19:0] data_in,
+    output reg [19:0] data_out
+);
+
+always @(posedge clock) begin
+    //more contents stored in register Ro into register Rd
+    data_out[Rd] <= data_in[Ro];
+end
+endmodule
+
+module LDC (
+    input clock,
+    input [19:0] R, //register R
+    input  [19:0] C, //constant C
+    output [19:0] data_out
+
+);
+    
+    always @(posedge clock) begin
+        //load numerical constant C directly into register R.
+        data_out[R] <= C;
+    end
+endmodule
+
+module LDD (
+    input clock,
+    input [19:0] Rd,
+    input [19:0] Ro,
+    input [19:0] mem_address,
+    input [19:0] [19:0]data_in,
+    output reg [19:0] data_out
+);
+
+always @(posedge clock) begin
+    data_out[Rd] <= data_in [Ro][mem_address];
+end
+    
+endmodule
+
+module STD (
+    input clock,
+    input [19:0] Rd,
+    input [19:0] Ro,
+    input [19:0] mem_address,
+    input [19:0] data_in,
+    output reg [19:0] [19:0] data_out
+
+);
+
+always @(posedge clock) begin
+    data_out[Rd][mem_address] <= data_in[Ro];
+end
+
+endmodule
--- urcpu.v
+++ urcpu.v
@@ -1,8 +1,11 @@
 `include "ALU.v"
 `include "registers.v"
+`include "control.v"
+`include "memory.v"
 
 module UrCPU (
     input clock,
 );
   
 endmodule
+
